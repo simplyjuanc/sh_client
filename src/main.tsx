2662b34cd93cfe5c434d4e844b8e7e2e1574ebb2
Juan Vasquez <37302562+simplyjuanc@users.noreply.github.com>
2025-04-13 16:27:46 +0100

    feat: update styling by removing Radix UI themes and adding global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import '@radix-ui/themes/styles.css';
+import './styles/globals.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Theme } from '@radix-ui/themes';
 
 const client = new QueryClient();
 
@@ -17,15 +16,8 @@ declare module '@tanstack/react-router' {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Theme
-      appearance='dark'
-      accentColor='grass'
-      grayColor='sand'
-      radius='large'
-    >
-      <QueryClientProvider client={client}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </Theme>
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
